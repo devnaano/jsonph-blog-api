45e9905b19cc86778091af40570047f64941ec5f
devnaano <naano.dev@gmail.com>
2023-11-16 16:27:27 -0300

    consuming the API, using the POST method to add a new post with the input values

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,6 @@ async function readPosts() {
 
     let response = await fetch('https://jsonplaceholder.typicode.com/posts');
     let json = await response.json();
-    console.log(json);
 
     if (json.length > 0) {
         postArea.innerHTML = '';
@@ -21,9 +20,58 @@ async function readPosts() {
         postArea.innerHTML = 'Nenhum post para exibir';
     }
     
-    // let createDiv = document.createElement('div');
-    // createDiv.classList.add('posts');
-    // document.body.appendChild(createDiv);
+    // let createDivEl = document.createElement('div');
+    // createDivEl.classList.add('posts');
+    // let createH2El = document.createElement('h2');
+    // createH2El.innerHTML = `${json[i].title}`;
+    // createDivEl.appendChild(createH2El);
+    // createPEl = document.createElement('p');
+    // createPEl.innerHTML = `${json[i].body}`;
+    // createDivEl.appendChild(createPEl);
+    // postArea.appendChild(createDiv);
 }
+
+
+async function addNewPost(title, body) {
+    await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                //NO JS QUANDO TEM UM OBJETO QUE A PROPRIEDADE É O MESMO NOME DE UMA VARIAVEL QUE ESTA SENDO INSERIDA VOCE PODE SIMPLISMENTE COLOCAR UMA VEZ QUE ELE ENTENDE
+                // METODO PADRÃO
+                // title: title,
+                // body: body,
+                // userId: 2
+
+                //JAVASCRIPT TBM ENTENDE
+                title,
+                body,
+                userId: 2
+            })
+        }
+    );
+
+    document.querySelector('#titleField').value = '';
+    document.querySelector('#bodyField').value = '';
+
+    readPosts();
+}
+
+
+
+document.querySelector('#insertButton').addEventListener('click', () => {
+    let title = document.querySelector('#titleField').value;
+    let body = document.querySelector('#bodyField').value;
+
+    if (title && body) {
+        addNewPost(title, body);
+    } else {
+        alert("Preencha todos os campos!");
+    }
+})
 
 readPosts();
